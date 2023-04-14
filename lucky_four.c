2537aeb823f8b49b8ec5354dc5a7aca0d962a408
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-14 10:02:51 +0530

    Update lucky_four.c

--- lucky_four.c
+++ lucky_four.c
@@ -1,3 +1,19 @@
+/*
+
+Read problems statements in Mandarin Chinese and Russian.
+
+You are given a list of T integers, for each of them you have to calculate the number of occurrences of the digit 4 in the decimal representation.
+
+Input
+The first line of input consists of a single integer T, denoting the number of integers in the list.
+
+Then, there are T lines, each of them contain a single integer from the list.
+
+Output
+Output T lines. Each of these lines should contain the number of occurrences of the digit 4 in the respective integer from the list.
+
+*/
+
 #include<stdio.h>
 int main()
 {
