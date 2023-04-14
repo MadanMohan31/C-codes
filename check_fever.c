c68b144aaa9f22f29e15dbb55983916fe36579a5
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-14 09:46:47 +0530

    Update check_fever.c

--- check_fever.c
+++ check_fever.c
@@ -1,3 +1,16 @@
+/*
+
+Chef is not feeling well today. He measured his body temperature using a thermometer and it came out to be 
+X °F.
+
+A person is said to have fever if his body temperature is strictly greater than 
+98
+98 °F.
+
+Determine if Chef has fever or not.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
