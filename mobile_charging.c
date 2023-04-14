40d9f1256a72513f6f9ecf868c9c37906e4c9b1c
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-14 09:56:12 +0530

    Update mobile_charging.c

--- mobile_charging.c
+++ mobile_charging.c
@@ -1,3 +1,46 @@
+/*
+
+Chef's phone shows a Battery Low notification if the battery level is 
+15
+%
+15% or less.
+
+Given that the battery level of Chef's phone is 
+�
+%
+X%, determine whether it would show a Battery low notification.
+
+Input Format
+First line will contain 
+�
+T, number of test cases. Then the test cases follow.
+Each test case contains a single line of input, an integer 
+�
+X, denoting the battery level of the phone.
+Output Format
+For each test case, output in a single line 
+Yes
+Yes, if the battery level is 
+15
+%
+15% or below. Otherwise, print 
+No
+No.
+
+You may print each character of 
+Yes
+Yes and 
+No
+No in uppercase or lowercase (for example, 
+YeS
+YeS, 
+YES
+YES, 
+yes
+yes will be considered identical).
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
