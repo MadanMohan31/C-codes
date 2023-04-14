daa6e324df4b052b5d0a8c8523dbbd6527c5965b
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-14 09:53:38 +0530

    Update withdrawal.c

--- withdrawal.c
+++ withdrawal.c
@@ -1,3 +1,23 @@
+/*
+
+Pooja would like to withdraw X $US from an ATM. The cash machine will only accept the transaction if X is a multiple of 5, and Pooja's account balance has enough cash to perform the withdrawal transaction (including bank charges). For each successful withdrawal the bank charges 0.50 $US. Calculate Pooja's account balance after an attempted transaction.
+
+Input Format
+Each input contains 2 integers 
+�
+X and 
+�
+Y. \
+�
+X is the amount of cash which Pooja wishes to withdraw. \
+�
+Y is Pooja's initial account balance.
+
+Output Format
+Output the account balance after the attempted transaction, given as a number with two digits of precision. If there is not enough money in the account to complete the transaction, output the current bank balance.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
