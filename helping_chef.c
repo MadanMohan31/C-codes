c1784671558387083a8ad6192aaccaaa64b4d498
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-14 09:43:47 +0530

    Create helping_chef.c

---
+++ helping_chef.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int t,i;
+    int n;
+    scanf("%d",&t);
+    for(i=1;i<=t;i++){
+   scanf("%d",&n);
+   if(n<10){
+       printf("Thanks for helping Chef!\n"); 
+   }
+   else{
+       printf("%d\n",(-1));
+   }
+    }
+      return 0; 
+   }
