660adfa500ac54ae799e9042e52a6617a255f911
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-14 09:52:43 +0530

    Update hot_or_cold.c

--- hot_or_cold.c
+++ hot_or_cold.c
@@ -1,3 +1,25 @@
+/*
+
+Chef considers the climate HOT if the temperature is above 
+20
+20, otherwise he considers it COLD. You are given the temperature 
+�
+C, find whether the climate is HOT or COLD.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+The first and only line of each test case contains a single integer, the temperature 
+�
+C.
+Output Format
+For each test case, print on a new line whether the climate is HOT or COLD.
+
+You may print each character of the string in either uppercase or lowercase (for example, the strings hOt, hot, Hot, and HOT will all be treated as identical).
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
