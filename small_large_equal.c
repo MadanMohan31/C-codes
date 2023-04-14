f081953fe63d3c28f93c0d930337dab0dc18851a
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-14 09:45:14 +0530

    Update small_large_equal.c

--- small_large_equal.c
+++ small_large_equal.c
@@ -1,3 +1,24 @@
+/*
+
+Write a program which prints small/large/equal relation of given two integers a and b.
+
+Input
+Two integers a and b separated by a single space are given in a line.
+
+Output
+For given two integers a and b, print
+
+a < b
+if a is less than b,
+
+a > b
+if a is greater than b, and
+
+a == b
+if a equals to b.
+
+*/
+
 #include <stdio.h>
 
 int main()
