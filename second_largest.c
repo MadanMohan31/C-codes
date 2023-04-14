7d805aff57101e9f222000f28f1fe4353a5b5fdb
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-14 10:00:52 +0530

    Update second_largest.c

--- second_largest.c
+++ second_largest.c
@@ -1,3 +1,47 @@
+/*
+
+Write a program to take two numbers as input and print their difference if the first number is greater than the second number 
+�
+�
+ℎ
+�
+�
+�
+�
+�
+�
+otherwise print their sum.
+
+Input Format
+First line will contain the first number (
+�
+1
+N1)
+Second line will contain the second number (
+�
+2
+N2)
+Output Format
+Output a single line containing the difference of 2 numbers 
+(
+�
+1
+−
+�
+2
+)
+(N1−N2) if the first number is greater than the second number otherwise output their sum 
+(
+�
+1
++
+�
+2
+)
+(N1+N2).
+
+*/
+
 #include<stdio.h>
 int main()
 {
